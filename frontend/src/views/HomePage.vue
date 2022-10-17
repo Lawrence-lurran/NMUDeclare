b1b3a8399b5970c64daa1b2b2b33f3dfd4dd9eb4
smileHan2333 <1335391958@qq.com>
2022-10-17 17:36:37 +0800

    [fix]:首页布局优化，视频播放功能

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -15,13 +15,31 @@
         <div class="mid11-title">
           <span>德育专题</span>
         </div>
-        <div class="mid11-content"></div>
+        <div class="mid11-content">
+          <div class="cycle-img">
+            <img src="~../assets/xcsp99.jpg" style="width: 100%; height: 100%" />
+            <div class="cycle-img-btns">
+              <div class="cycle-img-btn">1</div>
+              <div class="cycle-img-btn">2</div>
+              <div class="cycle-img-btn">3</div>
+              <div class="cycle-img-btn">4</div>
+              <div class="cycle-img-btn">5</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="mid12">
         <div class="mid12-title">
           <span>创新专栏</span>
         </div>
-        <div class="mid12-content"></div>
+        <div class="mid12-content">
+          <ul>
+            <li><a target="_blank" href="http://aqjxcg.xupt.edu.cn/cxhd1/xkjs.htm" title="学生创新成果">学生创新成果</a></li>
+            <li><a target="_blank" href="http://aqjxcg.xupt.edu.cn/info/1063/1309.htm" title="创新教育">创新教育</a></li>
+            <li><a target="_blank" href="http://aqjxcg.xupt.edu.cn/info/1063/1316.htm" title="网络空间安全特长班">网络空间安全特长班</a></li>
+            <li><a target="_blank" href="http://aqjxcg.xupt.edu.cn/info/1063/1320.htm" title="组织学科竞赛">组织学科竞赛</a></li>
+          </ul>
+        </div>
       </div>
     </div>
     <div class="mid2">
@@ -34,17 +52,17 @@
       </div>
     </div>
     <div class="mid3">
-      <!-- <div class="mid31" style="background: #000000 !important">
-        <video poster="images/v.jpg" width="340px" height="380px" src="images/v2.mp4" controls="controls" autoplay="">
-          <source src="images/v2.mp4" type="video/mp4" />
+      <div class="mid31" style="background: #000000 !important">
+        <video poster="~../assets/video_poster.jpg" width="340px" height="380px" src="~../assets/video.mp4" controls="controls">
+          <source src="~../assets/video.mp4" type="video/mp4" />
           <span style="margin-top: 150px; color: #ffffff; display: block; padding: 0 10px">浏览器版本太低，视频无法正常显示，请升级您的浏览器。</span>
         </video>
       </div>
       <div class="mid32">
-        <a href="http://aqjxcg.xupt.edu.cn/liuyan.jsp?urltype=tree.TreeTempUrl&amp;wbtreeid=1056" target="_blank"
-          ><img src="./西安邮电大学高等教育国家级教学成果奖申报网站_files/zxly.jpg"
-        /></a>
-      </div> -->
+        <a href="http://aqjxcg.xupt.edu.cn/liuyan.jsp?urltype=tree.TreeTempUrl&amp;wbtreeid=1056" target="_blank">
+          <img src="~../assets/online_call.jpg" />
+        </a>
+      </div>
     </div>
     <div class="mid4">
       <div class="mid4-title">代表性成果</div>
@@ -63,6 +81,13 @@ export default {
     return {
       result_summary1: `信息安全是国家安全的重要组成部分，习近平总书记指出我国信息安全要“战略清晰，技术先进，产业领先，攻防兼备”，并明确指出：“网络空间的竞争，归根结底是人才的竞争。建设网络强国，没有一支优秀的人才队伍，没有人才创造力迸发，活力涌流，是难以成功的。”西安邮电大学作为西北地区唯一一所以邮电通信技术为特色的高等院校，始终以国家信息安全战略需求为导向，从2004年就开始了信息安全专业招生和人才培养工作，累计培养毕业生1000余人。`,
       result_summary2: `我们秉承<strong>“德育引领，创新驱动，面向行业”</strong>的人才培养理念，信息安全人才培养面向地方信息化建设和信息通信行业需求，围绕中国特色网络安全观，通过理念、队伍、体系、内容、措施等方面的创新构建了“12345”德育教育体系；立足国家网络强国战略，通过目标、队伍、途径、平台、措施等方面的创新实施了“12345”创新教育体系；针对行业和地方需求，通过定位、特色、模块、主线、措施等方面的创新制定了“12345”特色人才培养体系，形成了“三位一体”的人才培养模式，注重学生德育教育、创新能力与攻防兼备能力的培养，信息安全特色人才培养成效显著，具有良好应用效果和示范效应。`,
+      cycle_imgs: [
+        { src: '~../assets/xcsp99.jpg' },
+        { src: '~../assets/rm_bj.jpg' },
+        { src: '~../assets/xcsp99.jpg' },
+        { src: '~../assets/rm_bj.jpg' },
+        { src: '~../assets/xcsp99.jpg' },
+      ],
     };
   },
   // 计算属性，会监听依赖属性值随之变化
@@ -94,7 +119,7 @@ export default {
 }
 .mid1 {
   float: left;
-  width: 290px;
+  width: 300px;
   height: 467px;
   margin-top: 10px;
 
@@ -114,6 +139,41 @@ export default {
       border-bottom: 2px solid #0290e1;
     }
     .mid11-content {
+      position: relative;
+      width: 290px;
+      height: 200px;
+
+      .cycle-img {
+        width: 98%;
+        height: 90%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin-left: 5px;
+
+        .cycle-img-btns {
+          position: absolute;
+          display: flex;
+          bottom: 15px;
+          right: 10px;
+          height: 10px;
+          .cycle-img-btn {
+            width: 20px;
+            height: 20px;
+            margin-right: 10px;
+            border-radius: 5px;
+
+            text-align: center;
+            font-size: 15px;
+            color: rgb(255, 255, 255, 0.5);
+            background: rgb(102, 102, 102, 0.5);
+          }
+          .cycle-img-btn:hover {
+            background: rgb(204, 204, 204, 0.5);
+          }
+        }
+      }
     }
   }
   .mid12 {
@@ -131,12 +191,35 @@ export default {
       border-bottom: 2px solid #0290e1;
     }
     .mid12-content {
+      width: 260px;
+      margin: 5px 0;
+
+      ul {
+        margin-top: 0;
+      }
+      li {
+        width: 260px;
+        height: 22px;
+        list-style: none;
+        line-height: 22px;
+        font-size: 12px;
+        padding: 0 0 0 15px;
+        margin-left: -30px;
+        background: url(../assets/array.gif) no-repeat left 6px;
+      }
+      ul li a {
+        text-decoration: none;
+        color: #000000;
+      }
+      ul li a:hover {
+        color: #fd8005;
+      }
     }
   }
 }
 .mid2 {
   float: left;
-  width: 330px;
+  width: 340px;
   height: 467px;
   margin-top: 10px;
   margin-left: 10px;
@@ -155,8 +238,8 @@ export default {
     font-family: '微软雅黑';
     font-size: 12px;
     text-indent: 2em;
-    line-height: 21px;
-    padding: 5px 5px;
+    line-height: 22px;
+    padding: 8px 8px;
 
     p {
       margin: 0;
@@ -169,9 +252,14 @@ export default {
   height: 467px;
   margin-top: 10px;
   margin-left: 10px;
-  border-radius: 10px;
 
-  background: white;
+  .mid31 {
+    // border-radius: 10px;
+  }
+  .mid32 {
+    margin-top: 10px;
+    border-radius: 10px;
+  }
 }
 .mid4 {
   float: left;
